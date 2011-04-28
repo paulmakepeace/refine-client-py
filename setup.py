35bb7754be1d843d6a91c9e84e698bbb6d7c63db
Paul Makepeace <Paul.Makepeace@realprogrammers.com>
2011-04-28 12:13:19 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""python setup.py install"""
+
+from distutils.core import setup
+
+setup(name='refine-client-py',
+      version='0.1',
+      description='Google Refine Python Client',
+      author='Paul Makepeace',
+      author_email='paulm@paulm.com',
+      url='https://github.com/PaulMakepeace/refine-client-py',
+      packages=[],
+)
