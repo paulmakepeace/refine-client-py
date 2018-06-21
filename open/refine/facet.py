bb13a7264c8d1300597cd737906a06d24193c73c
Daniel Butler windows <dabutler89@gmail.com>
2018-06-21 17:01:27 -0400

    update to comply with pep8

--- open/refine/facet.py
+++ open/refine/facet.py
@@ -18,7 +18,6 @@ OpenRefine Facets, Engine, and Facet Responses.
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-import json
 import re
 
 
